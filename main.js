a52ad603b2224d2e330f71e051a9e2088bd8eef5
EA - Raul Egas Do Prado Sobrinho <raul_egas@educadventista.org>
2025-05-22 13:40:01 -0300

    aula 3 em sala

---
+++ main.js
@@ -0,0 +1,18 @@
+//array
+const items = []
+
+function addItem(){
+    const itemName = document.querySelector("#item").value
+
+//objeto
+    const item = {
+        name: itemName,
+        checked: false
+    }
+
+    items.push(item)
+
+    document.querySelector("#item").value = ""
+
+    console.log(items)
+}
