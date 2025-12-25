9bb163b1a6a7322ca0e5c15ea0381ddcf13f00ef
Raul Egas <raul.sobrinho@eaportal.org>
2025-12-25 20:45:11 -0300

    criação das funções que criam e buscam o filtro no banco de dados

---
+++ index.js
@@ -0,0 +1,63 @@
+const linkAPI = "http://localhost:3333"
+
+const buttonBuscar = document.querySelector(".buscar")
+buttonBuscar.onclick = () => {
+    filterSearch()
+}
+
+const buttonCriar = document.querySelector(".criar")
+buttonCriar.onclick = () => {
+    createFilter()
+}
+
+async function createFilter() {
+    const filtro = document.querySelector("#filtro").value
+
+    if (!filtro) {
+        alert("Preencha o campo do banco de dados")
+        return
+    }
+
+    const response = await fetch(`${linkAPI}/criarFiltro`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ filtro })
+    }).then(response => response.json())
+
+    const {message, error} = response
+
+    if(error){
+        alert(error)
+        return
+    }
+
+    alert(message)
+}
+
+async function filterSearch() {
+    const filtro = document.querySelector("#filtro").value
+
+    if (!filtro) {
+        alert("Preencha o campo do banco de dados!")
+        return
+    }
+
+    const response = await fetch(`${linkAPI}/pesquisaFiltro`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ filtro })
+    }).then(response => response.json())
+
+    const { message, error } = response
+
+    if (error) {
+        alert(error)
+        return
+    }
+
+    alert(message)
+}
